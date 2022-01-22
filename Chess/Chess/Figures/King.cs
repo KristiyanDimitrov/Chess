b31081548bc8afd2e9f577248f5d73171ca492ae
Kristiyan Dimitrov <Kristiyan_D@outlook.com>
2022-01-22 20:33:29 +0000

    progress

--- Chess/Chess/Figures/King.cs
+++ Chess/Chess/Figures/King.cs
@@ -9,6 +9,8 @@ namespace Chess.Figures
     {
         public King(int row, int column, ColorList color) : base(row, column, color) { }
         public bool KingInCheck { get; set; } = false;
+        private bool _castleMovePossible = false;
+        public bool CastleMovePossible { get { return _castleMovePossible; } set { _castleMovePossible = false; } }
         public override List<Position> PossibleMoves(Board board)
         {
             List<Position> PossiblePositions = new List<Position>();
@@ -16,10 +18,25 @@ namespace Chess.Figures
             int X = CurPos.Row;
             int Y = CurPos.Column;
 
-
+            // Normal moves
             for (int x = -1; x <= 1; x++)
                 for (int y = -1; y <= 1; y++)
-                        board.BasicMoveValidate(PossiblePositions, this, X + x, Y + y);
+                    board.BasicMoveValidate(PossiblePositions, this, X + x, Y + y);
+
+            // Castle move. The validation for fields under attack is done outside of this class in Game.GetPossibleMoves().
+            if (FirstMove && !KingInCheck)
+            {
+                for (int y = Y; y >= 0; y--)
+                {
+                    Figure figure = board.GetFigureFromPosition(X, y);
+                    if (figure is null)
+                        continue;
+                    if (figure != null && figure is Rook && ((Rook)figure).IsFirstMove)
+                        if (board.BasicMoveValidate(PossiblePositions, this, X, Y + 2))
+                            _castleMovePossible = true;
+                }
+            }
+
 
             return PossiblePositions;
         }
@@ -37,5 +54,7 @@ namespace Chess.Figures
 
         public override string ToString() => "K";
         private bool FirstMove = true;
+        private bool _CastleMovePossible;
+        private bool castleMovePossible = false;
     }
 }
--- Chess/Chess/Game.cs
+++ Chess/Chess/Game.cs
@@ -77,26 +77,35 @@ namespace Chess
             KingInCheckUpdate(CurrentPlayer);
         }
 
-        public List<Position> getPossibleMoves(Position from)
+        public List<Position> GetPossibleMoves(Position from)
         {
             Figure SelectedFigure = Chessboard.GetFigureFromPosition(from);
             List<Position> PossibleMoves = SelectedFigure.PossibleMoves(Chessboard);
 
-            //Remove moves that will put frendly King in Check
-            KingCheck(SelectedFigure.Color, SelectedFigure, PossibleMoves);
+            // Castle move special case. Prevent Castle move if the Kings path is under attack.
+            bool ToCastleCheck = SelectedFigure is King && ((King)SelectedFigure).CastleMovePossible ? true : false;
+
+            
+            // Position CastleMove = PossibleMoves.FirstOrDefault(x => Math.Abs(x.Column - SelectedFigure.GettPosition().Column) == 2);
+
+            KingCheck(SelectedFigure.Color, SelectedFigure, PossibleMoves, ToCastleCheck);//Remove moves that will put friendly King in Check
 
             return PossibleMoves;
         }
 
         /*
-            This method uses "ShadowMoves" to check the consequences of a move. A ShadowMove consists of making the move in a controlled and reversable way,
+            This method uses "ShadowMoves" to check the consequences of a move. A ShadowMove consists of making the move in a controlled and reversible way,
         in order to use the existing methods of checking possible moves. 
-            This helps to check if the state of the board after a move is valid. For example if it puts a friendly king in danger.           
+            This helps to check if the state of the board after a move is valid. For example if it puts a friendly king in danger.
+        
+            This method is also checks if friendly king is in danger during the Castle move.
         */
-        private void KingCheck(Figure.ColorList color, Figure selectedFigure, List<Position> moves)
+        private void KingCheck(Figure.ColorList color, Figure selectedFigure, List<Position> moves, bool toCastleCheck = false)
         {
-            Position TheKingPos = Chessboard.GetKingFigure(color).GettPosition();
+            Position TheKingPos = Chessboard.GetKingFigure(color).GettPosition(); // When the figure to move is the King this breaks the logic ¬¬¬¬¬¬¬
             List<Position> MovesToRemove = new List<Position>();
+
+            Position CastleMove = moves.FirstOrDefault(x => Math.Abs(x.Column - SelectedFigure.GettPosition().Column) == 2);
 
             foreach (Position pos in moves)
             {
@@ -110,9 +119,11 @@ namespace Chess
                         if (CurFigure == null || CurFigure?.Color == color)
                             continue;
 
-                        // If the potental move puts the frendly King in danger remove it from the list of possible moves.
+                        // If the potential move puts the friendly King in danger remove it from the list of possible moves.
                         List<Position> CurFigurePossibleMoves = CurFigure.PossibleMoves(Chessboard);
                         if (CurFigurePossibleMoves.Exists(move => move.Column == TheKingPos.Column && move.Row == TheKingPos.Row))
+                            if(toCastleCheck && )
+                                
                             MovesToRemove.Add(pos);
                     }
                 }
@@ -124,7 +135,7 @@ namespace Chess
                 moves.RemoveAll(x => x.Column == move.Column && x.Row == move.Row);
         }
 
-        // Check if the played move puts the oponents King in Check
+        // Check if the played move puts the opponents King in Check
         private void KingInCheckUpdate(Player CurentPlayer)
         {
             King TheKing = (King)Chessboard.GetKingFigure(CurrentPlayer.Color == Figure.ColorList.Black ? Figure.ColorList.White : Figure.ColorList.Black);
@@ -140,7 +151,7 @@ namespace Chess
                     List<Position> CurFigurePossibleMoves = CurFigure.PossibleMoves(Chessboard);
                     if (CurFigurePossibleMoves.Exists(move => move.Column == TheKing.FigurePosition.Column && move.Row == TheKing.FigurePosition.Row))
                     {
-                        TheKing.KingInCheck = true; // Don't like the check being in two places, will be like this for now untill I figure out how to implement it best ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+                        TheKing.KingInCheck = true; // Don't like the check being in two places, will be like this for now until I figure out how to implement it best ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
                         CurentPlayer.InCheck = true;
                         return;
                     }
